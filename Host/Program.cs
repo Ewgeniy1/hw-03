e5ee8db0c9a58e7a5b900631b258c350b8ddce13
Bismarc <Bismarc@BISMARC>
2023-04-06 23:30:16 +0300

    Добавьте файлы проекта.

---
+++ Host/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+
+namespace Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int A;
+            A = int.Parse(Console.ReadLine());
+            if (A == 1 || A == 2 || A == 12)
+            {
+                Console.WriteLine("\n You have chosen the winter months");
+            }
+            else if (A == 3 || A == 4 || A == 5)
+            {
+                Console.WriteLine("\n You have chosen the spring months");
+            }
+            else if (A == 6 || A == 7 || A == 8)
+            {
+                Console.WriteLine("\n You have chosen the summer months");
+
+            }
+            else if (A == 9 || A == 10 || A == 11)
+            {
+                Console.WriteLine("\n You have chosen the autumn months");
+            }
+            switch (A)
+            {
+                case 1:
+                    Console.WriteLine(Lang.january);
+
+                    break;
+                case 2:
+                    Console.WriteLine(Lang.february);
+                    break;
+                case 3:
+                    Console.WriteLine(Lang.march);
+                    break;
+                case 4:
+                    Console.WriteLine(Lang.april);
+                    break;
+                case 5:
+                    Console.WriteLine(Lang.may);
+                    break;
+                case 6:
+                    Console.WriteLine(Lang.june);
+                    break;
+                case 7:
+                    Console.WriteLine(Lang.july);
+                    break;
+                case 8:
+                    Console.WriteLine(Lang.august);
+                    break;
+                case 9:
+                    Console.WriteLine(Lang.september);
+                    break;
+                case 10:
+                    Console.WriteLine(Lang.october);
+                    break;
+                case 11:
+                    Console.WriteLine(Lang.november);
+                    break;
+                case 12:
+                    Console.WriteLine(Lang.december);
+                    break;
+            }
+
+            Console.WriteLine();
+
+        }
+
+    }
+
+}
+
+
+            
+        
+    
---
+++ Lang/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Lib 
+{
+}
+
+
+
+
+public class Lang
+{
+    public const string january = "\n January.\n \n Winter month.\n \n in it for 31 days";
+    public const string february = "\n February.\n \n Winter month.\n \n in it for 28 days";
+    public const string march = "\n Marc.\n \nSpring month.\n \n in it for 31 days";
+    public const string april = "\n Apri.\n \n Spring month.\n \n in it for 30 days";
+    public const string may = "\n May.\n \n Spring month.\n \n in it for 31 days";
+    public const string june = "\n June.\n \n Summer month.\n \n in it for 30 days";
+    public const string july = "\n July.\n \n Summer month.\n \n in it for 31 days";
+    public const string august = "\n August.\n \n Summer month.\n \n in it for 31 days";
+    public const string september = "\n September.\n \n Autumn month.\n \n in it for 30 days";
+    public const string october = "\n October.\n \n Autumn month.\n \n in it for 31 days";
+    public const string november = "\n November.\n \n Autumn month.\n \n in it for 30 days ";
+    public const string december = "\n December.\n \n Winter month.\n \n in it for 31 days";
+
+
+
+
+
+    static void Main() { }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
